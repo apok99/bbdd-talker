33afaf5b589ce53f2f2d7470522b7b10d1ddaead
DAVID VICTORIA <apok9999@hotmail.com>
2025-10-21 13:06:46 +0200

    Restore default service provider registration

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\ServiceProvider;
+
 return [
     'name' => env('APP_NAME', 'BBDD Talker'),
     'env' => env('APP_ENV', 'local'),
@@ -11,19 +13,10 @@ return [
     'fallback_locale' => 'en',
     'key' => env('APP_KEY'),
     'cipher' => 'AES-256-CBC',
-    'providers' => require base_path('bootstrap/providers.php'),
-    'aliases' => [
-        'App' => Illuminate\Support\Facades\App::class,
-        'Arr' => Illuminate\Support\Arr::class,
-        'Auth' => Illuminate\Support\Facades\Auth::class,
-        'Blade' => Illuminate\Support\Facades\Blade::class,
-        'Cache' => Illuminate\Support\Facades\Cache::class,
-        'Config' => Illuminate\Support\Facades\Config::class,
-        'DB' => Illuminate\Support\Facades\DB::class,
-        'Log' => Illuminate\Support\Facades\Log::class,
-        'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
-        'Session' => Illuminate\Support\Facades\Session::class,
-        'View' => Illuminate\Support\Facades\View::class,
-    ],
+    'providers' => ServiceProvider::defaultProviders()
+        ->merge(require base_path('bootstrap/providers.php'))
+        ->toArray(),
+    'aliases' => ServiceProvider::defaultAliases()->merge([
+        //
+    ])->toArray(),
 ];
